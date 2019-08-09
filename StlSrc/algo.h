66c8c5ec6347418ac09be101c4ae6ba27184663c
CrazyKing5023 <18243089835@163.com>
2019-08-09 20:09:41 +0800

    小型STL库

---
+++ StlSrc/algo.h
@@ -0,0 +1,8 @@
+//
+// Created by jerry on 8/8/19.
+//
+
+#ifndef TINYSTL_ALGO_H
+#define TINYSTL_ALGO_H
+
+#endif //TINYSTL_ALGO_H
---
+++ StlSrc/vector.h
@@ -0,0 +1,8 @@
+//
+// Created by jerry on 8/8/19.
+//
+
+#ifndef TINYSTL_VECTOR_H
+#define TINYSTL_VECTOR_H
+
+#endif //TINYSTL_VECTOR_H
