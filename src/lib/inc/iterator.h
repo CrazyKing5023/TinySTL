c35de2e0f86a354f219f046e5fc336d14581745f
CrazyKing5023 <18243089835@163.com>
2019-09-17 23:33:23 +0800

    iterator

--- src/lib/inc/iterator.h
+++ src/lib/inc/iterator.h
@@ -5,7 +5,12 @@ namespace tinystl
 {
     struct InputIteratorTag{};
     struct OutputIteratorTag{};
+    struct ForwardIteratorTag : public InputIteratorTag{};
+    struct BidirectionalIteratorTag : public ForwardIteratorTag{};
+    struct RandomAccessIteratorTag : public BidirectionalIteratorTag{};
+
     
+
 
 }
 
